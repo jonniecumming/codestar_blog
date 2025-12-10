2de7e34547752e6b513fa8b0f6b9ff789e04e0e4
jonniecumming <jonniecumming@gmail.com>
2025-12-10 15:03:27 +0000

    Update DATABASE_URL in env.py for new database configuration

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_4cktlghu8WJe@ep-autumn-field-agdh2ntb.c-2.eu-central-1.aws.neon.tech/crown_alibi_polar_628359")
+    "DATABASE_URL", "postgresql://neondb_owner:npg_4cktlghu8WJe@ep-autumn-field-agdh2ntb.c-2.eu-central-1.aws.neon.tech/gag_agony_debit_189334")
