798b5735b09a87eeaf0b5dbfff52452c641b67b7
jonniecumming <jonniecumming@gmail.com>
2025-12-10 15:50:01 +0000

    Remove env.py from tracking

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_4cktlghu8WJe@ep-autumn-field-agdh2ntb.c-2.eu-central-1.aws.neon.tech/gag_agony_debit_189334")
-os.environ.setdefault("SECRET_KEY", "Hv3^*3coihjrehoeriqcoqwierncowiue687643!OIUoihf")
