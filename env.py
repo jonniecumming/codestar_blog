0a4307e26d36e428d901bd794f107d05f5278c9b
jonniecumming <jonniecumming@gmail.com>
2025-12-10 15:41:37 +0000

    Add secret key example to test visibility

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_4cktlghu8WJe@ep-autumn-field-agdh2ntb.c-2.eu-central-1.aws.neon.tech/gag_agony_debit_189334")
+os.environ.setdefault("SECRET_KEY", "Hv3^*3c")
