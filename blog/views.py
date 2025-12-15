6a3c2e7b42cc24701bd8ae78d30192fcfd4b0b10
jonniecumming <jonniecumming@gmail.com>
2025-12-15 15:42:35 +0000

    Create about app challenge

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,12 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+
 
 def post_detail(request, slug):
     """
@@ -24,4 +26,9 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    return render(request,"blog/post_detail.html",{"post": post},)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
