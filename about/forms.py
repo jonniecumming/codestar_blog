020358881aac78671e7b09071e883ab2a63b64c4
jonniecumming <jonniecumming@gmail.com>
2025-12-17 14:54:09 +0000

    Refactor CollaborateForm fields for improved readability and update about_me view for collaboration request handling

--- about/forms.py
+++ about/forms.py
@@ -1,7 +1,12 @@
 from .models import CollaborateRequest
 from django import forms
 
+
 class CollaborateForm(forms.ModelForm):
     class Meta:
         model = CollaborateRequest
-        fields = ('name', 'email', 'message',)
+        fields = (
+            "name",
+            "email",
+            "message",
+        )
