06643f16ad06b42897aa2f65ff5d5e8e6ef06972
jonniecumming <jonniecumming@gmail.com>
2025-12-19 15:26:31 +0000

    Enhance About and Blog models with profile image support, improve admin display, and update templates for better image handling

--- about/models.py
+++ about/models.py
@@ -1,18 +1,26 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 
 class About(models.Model):
+    """
+    Stores a single about me text.
+    """
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
+    profile_image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return self.title
 
 
 class CollaborateRequest(models.Model):
+    """
+    Stores a single collaboration request message.
+    """
     name = models.CharField(max_length=200)
     email = models.EmailField()
     message = models.TextField()
